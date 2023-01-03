de3ca5ce237e8935bb7163a42ac2fbe0c10ae211
emoboybd <shahedurrahman.shakil53@gmail.com>
2023-01-03 16:31:37 +0600

    initial commit

---
+++ src/components/Row.js
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from "react";
+import axios from "../axios";
+import "./Row.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
+
+const baseUrl = "https://image.tmdb.org/t/p/original/";
+
+const Row = ({ title, fetchUrl, isLargeRow }) => {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+
+  // A snippet of code which runs based on a specific sondition/variable.
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+
+      return request;
+    }
+
+    fetchData();
+  }, [fetchUrl]);
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    console.log(movie);
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(
+        movie?.name || movie?.original_title || movie?.original_name || ""
+      )
+        .then((url) => {
+          // to get everything after the question mark. urlParams.get('V') will give us everything after the v= so this 'qV-5agogMdM'
+          // https://www.youtube.com/watch?v=qV-5agogMdM
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  };
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row__posters">
+        {/*several row__poster(s) */}
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            onClick={() => handleClick(movie)}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${baseUrl}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+    </div>
+  );
+};
+
+export default Row;
